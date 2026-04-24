f8f5aaee0dd4e0445c1b5903f9f456e057199dcc
iknevo <nevoexists@gmail.com>
2026-04-24 08:07:47 +0300

    sync

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -15,3 +15,41 @@ return {
     },
   },
 }
+
+-- return {
+--   {
+--     "slugbyte/lackluster.nvim",
+--     priority = 1000,
+--     config = function()
+--       require("lackluster").setup({
+--         disable_plugin = {},
+--         tweak_ui = {
+--           disable_undercurl = true,
+--           enable_end_of_buffer = true,
+--         },
+--         tweak_syntax = {
+--           comment = require("lackluster").color.gray6,
+--           string = require("lackluster").color.orange,
+--           string_escape = require("lackluster").color.gray1,
+--           builtin = "default",
+--           type = "default",
+--           keyword = "default",
+--           keyword_return = "default",
+--           keyword_exception = "default",
+--         },
+--         tweak_background = {
+--           normal = "#000000",
+--           telescope = "#000000",
+--           menu = "#000000",
+--           popup = "#000000",
+--         },
+--       })
+--     end,
+--   },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "lackluster-hack",
+--     },
+--   },
+-- }
