db2e6d9fa1d4d303857e7d5e841d61347f358467
iknevo <nevoexists@gmail.com>
2026-05-26 03:03:45 +0300

    sync

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,20 +1,20 @@
 return {
   "lewis6991/gitsigns.nvim",
   opts = {
-    -- signs = {
-    --   add = { text = "+" },
-    --   change = { text = "~" },
-    --   delete = { text = "_" },
-    --   topdelete = { text = "‾" },
-    --   changedelete = { text = "~" },
-    -- },
-    -- signs_staged = {
-    --   add = { text = "+" },
-    --   change = { text = "~" },
-    --   delete = { text = "_" },
-    --   topdelete = { text = "‾" },
-    --   changedelete = { text = "~" },
-    -- },
+    signs = {
+      add = { text = "+" },
+      change = { text = "~" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+    },
+    signs_staged = {
+      add = { text = "+" },
+      change = { text = "~" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+    },
     signs_staged_enable = true,
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
     numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -29,7 +29,7 @@ return {
     current_line_blame_opts = {
       virt_text = true,
       virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-      delay = 1000,
+      delay = 500,
       ignore_whitespace = false,
       virt_text_priority = 100,
       use_focus = true,
