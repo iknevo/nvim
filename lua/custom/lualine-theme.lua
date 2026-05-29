6420e79a092907e3b1400452834a85b73a9d0b45
iknevo <nevoexists@gmail.com>
2026-05-29 15:49:38 +0300

    sync

--- lua/custom/lualine-theme.lua
+++ lua/custom/lualine-theme.lua
@@ -18,38 +18,38 @@ M.theme = function()
   return {
     inactive = {
       a = { fg = colors.gray, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.black },
-      c = { fg = colors.gray, bg = colors.black },
+      b = { fg = colors.gray, bg = "none" },
+      c = { fg = colors.gray, bg = "none" },
     },
 
     normal = {
       a = { fg = colors.darkgray, bg = colors.normal, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.black },
-      c = { fg = colors.gray, bg = colors.black },
+      b = { fg = colors.gray, bg = "none" },
+      c = { fg = colors.gray, bg = "none" },
     },
 
     insert = {
       a = { fg = colors.darkgray, bg = colors.insert, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.black },
-      c = { fg = colors.gray, bg = colors.black },
+      b = { fg = colors.gray, bg = "none" },
+      c = { fg = colors.gray, bg = "none" },
     },
 
     visual = {
       a = { fg = colors.darkgray, bg = colors.visual, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.black },
-      c = { fg = colors.gray, bg = colors.black },
+      b = { fg = colors.gray, bg = "none" },
+      c = { fg = colors.gray, bg = "none" },
     },
 
     replace = {
       a = { fg = colors.darkgray, bg = colors.replace, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.black },
-      c = { fg = colors.gray, bg = colors.black },
+      b = { fg = colors.gray, bg = "none" },
+      c = { fg = colors.gray, bg = "none" },
     },
 
     command = {
       a = { fg = colors.darkgray, bg = colors.command, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.black },
-      c = { fg = colors.gray, bg = colors.black },
+      b = { fg = colors.gray, bg = "none" },
+      c = { fg = colors.gray, bg = "none" },
     },
   }
 end
