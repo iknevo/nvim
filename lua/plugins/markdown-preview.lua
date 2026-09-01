a88622114018b9ff00e90a8650cc42149266b45e
iknevo <nevoexists@gmail.com>
2026-09-01 04:24:43 +0300

    sync

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -3,8 +3,9 @@ return {
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   build = "cd app && yarn install",
   init = function()
-    vim.g.mkdp_auto_close = 1
+    vim.g.mkdp_auto_close = 0 -- 1 auto closes the file when u change buffers, 0 doesn't
     vim.g.mkdp_refresh_slow = 0
+    vim.g.mkdp_echo_preview_url = 1
   end,
   config = function()
     vim.api.nvim_buf_create_user_command(0, "MarkdownPreview", function()
@@ -18,6 +19,11 @@ return {
     end, { force = true })
   end,
   keys = {
-    { "<leader>cp", "<cmd>MarkdownPreviewToggle<CR>", desc = "Markdown Preview Toggle" },
+    {
+      "<leader>cp",
+      "<cmd>MarkdownPreviewToggle<CR>",
+      ft = "markdown",
+      desc = "Markdown Preview Toggle",
+    },
   },
 }
